3d3ecadf1e1d50326213bd8151e5106457981adf
Raja Kolli <rajadilipkolli@gmail.com>
2026-09-20 14:52:47 +0000

    modifies gitignore

--- src/main/java/com/example/highrps/author/package-info.java
+++ src/main/java/com/example/highrps/author/package-info.java
@@ -1,13 +1,7 @@
 @ApplicationModule(
         displayName = "Author Management",
         type = ApplicationModule.Type.OPEN,
-        allowedDependencies = {
-            "shared",
-            "infrastructure",
-            "infrastructure::cache",
-            "infrastructure::redis",
-            "infrastructure::kafka"
-        })
+        allowedDependencies = {"shared", "infrastructure", "infrastructure::cache", "infrastructure::redis"})
 package com.example.highrps.author;
 
 import org.springframework.modulith.ApplicationModule;
